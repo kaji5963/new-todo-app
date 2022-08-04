b8ce1f8b5f8b93db900ef18457b71ae5c4df3f71
takuya_kajiwara <fd.elt.119@gmail.com>
2022-08-04 23:10:14 +0900

    fix-and-removefile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+export const App = () => {
+  const [todo, setTodo] = useState("");
+  const [makeTodo, setMakeTodo] = useState([]);
+  //フォームの送信処理をして初期化
+  const handleSubmit = (e) => {
+    e.preventDefault(); //フォーム送信時のレンダリングを防止
+    if (todo === "") return;
+    setMakeTodo([...makeTodo, { id: makeTodo.length + 1, text: todo }]);
+    setTodo(""); //フォームの初期化
+  };
+  //入力データの変更
+  const inputChange = (e) => {
+    setTodo(e.target.value); //valueのデータを取得
+  };
 
-function App() {
+  //予定TODOのデータを削除
+  const onClickDelete = (id) => {
+    const deleteTodo = makeTodo.filter((todo) => {
+      return todo.id !== id;
+    });
+    setMakeTodo(deleteTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 見出し */}
+      <h1>~ React TODO App ~</h1>
+      {/* フォーム */}
+      <form className="input-area" onSubmit={handleSubmit}>
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="TODO入力"
+          value={todo}
+        />
+        <button>追加</button>
+      </form>
+      {/* 予定TODO */}
+      <section className="schedule">
+        <p className="title"></p>
+        <div className="schedule-section">
+          <ul>
+            {makeTodo.map((todo) => {
+              return (
+                <div key={todo.id} className="schedule-item">
+                  <li>{todo.text}</li>
+                  <button>進行</button>
+                  <button onClick={() => onClickDelete(todo.id)}>削除</button>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
   );
-}
-
-export default App;
+};
