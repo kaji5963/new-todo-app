126433a83cbab162ca7d97bbc31812a5e4edfa54
takuya_kajiwara <fd.elt.119@gmail.com>
2022-08-07 14:24:05 +0900

    内容変更

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import React, { useState } from "react";
 import "./App.css";
 
 export const App = () => {
+  //各TODOを定義
+  const todoList = [
+    {
+      id: Math.floor(Math.random() * 300),
+      text: "",
+      status: "",
+    },
+    {
+      id: Math.floor(Math.random() * 300),
+      text: "",
+      status: "",
+    },
+    {
+      id: Math.floor(Math.random() * 300),
+      text: "",
+      status: "",
+    },
+  ];
+  const [schedule, progress, complete] = todoList; //各TODOを分割代入
+
   //useState宣言
   const [todo, setTodo] = useState("");
-  const [makeTodo, setMakeTodo] = useState([]);
+  const [makeTodo, setMakeTodo] = useState([
+    {
+      id: Math.floor(Math.random() * 300),
+      text: "",
+      status: "",
+    },
+  ]);
   //フォームの送信処理をして初期化
   const handleSubmit = (e) => {
     e.preventDefault(); //フォーム送信時のレンダリングを防止
     if (todo === "") return;
-    setMakeTodo([...makeTodo, { id: makeTodo.length + 1, text: todo }]);
+
+    setMakeTodo([
+      ...makeTodo,
+      {
+        id: Math.floor(Math.random() * 300),
+        text: todo,
+        status: "schedule",
+      },
+    ]);
     setTodo(""); //フォームの初期化
   };
   //入力データの変更
@@ -23,6 +57,21 @@ export const App = () => {
       return todo.id !== id;
     });
     setMakeTodo(deleteTodo);
+  };
+  //予定TODOのデータを進行TODOへ
+  const onClickProgress = (id) => {
+    const newProgressTodo = [
+      ...makeTodo,
+      {
+        id: Math.floor(Math.random() * 300),
+        text: todo,
+        status: "schedule",
+      },
+    ];
+    const deleteScheduleTodo = makeTodo.filter((todo) => {
+      return todo.id !== id;
+    });
+    setMakeTodo(deleteScheduleTodo);
   };
 
   return (
@@ -39,23 +88,66 @@ export const App = () => {
         />
         <button>追加</button>
       </form>
-      {/* 予定TODO */}
-      <section className="schedule">
-        <p className="title"></p>
-        <div className="schedule-section">
-          <ul>
-            {makeTodo.map((todo) => {
-              return (
-                <div key={todo.id} className="schedule-item">
-                  <li>{todo.text}</li>
-                  <button>進行</button>
-                  <button onClick={() => onClickDelete(todo.id)}>削除</button>
-                </div>
-              );
-            })}
-          </ul>
-        </div>
-      </section>
+      <div className="container">
+        {/* 予定TODO */}
+        <section className="schedule">
+          <p className="title">予定TODO</p>
+          <div className="schedule-section">
+            <ul>
+              {makeTodo.status !== "schedule"
+                ? makeTodo.map((todo) => {
+                    return (
+                      <div key={todo.id} className="schedule-item">
+                        <li>{todo.text}</li>
+                        <button onClick={() => onClickProgress(todo.id)}>
+                          進行
+                        </button>
+                        <button onClick={() => onClickDelete(todo.id)}>
+                          削除
+                        </button>
+                      </div>
+                    );
+                  })
+                : false}
+            </ul>
+          </div>
+        </section>
+        {/* 進行TODO */}
+        <section className="progress">
+          <p className="title">進行TODO</p>
+          <div key={todo.id} className="progress-section">
+            <ul>
+              {makeTodo.status !== "progress" &&
+                makeTodo.map((todo) => {
+                  return (
+                    <div key={todo.id} className="progress-item">
+                      <li>{todo.text}</li>
+                      <button>完了</button>
+                      <button>戻す</button>
+                    </div>
+                  );
+                })}
+            </ul>
+          </div>
+        </section>
+        {/* 完了TODO */}
+        <section className="complete">
+          <p className="title">完了TODO</p>
+          <div key={todo.id} className="complete-section">
+            <ul>
+              {makeTodo.map((todo) => {
+                return (
+                  <div key={todo.id} className="complete-item">
+                    <li>{todo.text}</li>
+                    <button>削除</button>
+                    <button>戻す</button>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+        </section>
+      </div>
     </>
   );
-};
+};;
